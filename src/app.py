b7602977fbfa09bfa8d1548c2df29073c46ca860
Jere-my98 <justinjerome401@gmail.com>
2022-10-11 12:38:11 +0300

    Solved Gitignore error - pythonenv

--- src/app.py
+++ src/app.py
@@ -1,10 +1,26 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def index():
-    return "Hello World!"
+fakeDatabase = {
+    1:{'name':'clean car'},
+    2:{'name':'Write Blog'},
+    3:{'name':'Start Stream'}
+}
 
-if __name__ == "__main__":
-    app.run()
+class Items(Resource):
+    def get(self):
+        return fakeDatabase
+
+class Item(Resource):
+    def get(self, pk):
+        return fakeDatabase[pk]
+
+api.add_resource(Items,'/')
+api.add_resource(Item,'/<int:pk>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
